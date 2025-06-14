2246b7f139b1456b9a6b6ced47844fbd3a7e7d83
Diego Sanchez Chuquimango <165108758+dsanchezchu@users.noreply.github.com>
2025-06-14 01:52:33 -0500

    Update S3.tf

--- S3.tf
+++ S3.tf
@@ -1,35 +1,58 @@
-resource "aws_s3_bucket" "demo_bucket" {
-  bucket = "demo-bucket-leonardo"
-
+# S3 bucket for frontend assets
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = "leocorp-frontend-${terraform.workspace}"
+  
   tags = {
-    Name        = "Demo Bucket Leonardo"
+    Name        = "Frontend Bucket"
+    Environment = terraform.workspace
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "demo_bucket_access" {
-  bucket = aws_s3_bucket.demo_bucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+# S3 bucket ACL
+resource "aws_s3_bucket_ownership_controls" "frontend_bucket_ownership" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
-resource "aws_s3_bucket_policy" "public_read" {
-  bucket = aws_s3_bucket.demo_bucket.id
-  depends_on = [aws_s3_bucket_public_access_block.demo_bucket_access]
-  
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject",
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = ["s3:GetObject"],
-        Resource  = "${aws_s3_bucket.demo_bucket.arn}/*"
-      }
-    ]
-   })
+resource "aws_s3_bucket_public_access_block" "frontend_bucket_access" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# S3 bucket policy for CloudFront access
+resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  policy = data.aws_iam_policy_document.s3_policy.json
+}
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.frontend_bucket.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.oai.iam_arn]
+    }
+  }
+}
+
+# S3 bucket website configuration
+resource "aws_s3_bucket_website_configuration" "frontend_bucket_website" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
 }
 
